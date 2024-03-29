b93e624a2517ae29cbbc991bc80a29b3a1843292
Marco-Romero <122071151+MazMorr@users.noreply.github.com>
2024-03-29 03:57:56 +0100

    Add files via upload

---
+++ candela.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class candela{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        System.out.println(x);
+        System.out.println("Hola Mundo");
+        sc.close();
+    }
+}
